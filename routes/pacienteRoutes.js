67ee42babf456ecd2a0fcb91597ae90651bef2be
MiriamOliver <miriamoliver99@gmail.com>
2023-06-01 21:10:27 +0200

    cambios rutas

--- routes/pacienteRoutes.js
+++ routes/pacienteRoutes.js
@@ -30,7 +30,22 @@ router.get('/listar/:dni', controlador.conseguirPaciente); //mostrar datos de un
 
 router.delete('/borrar/:dni', controlador.borrarPaciente); //borrar datos de un paciente por su dni
 
-router.put('/modificar/:dni', controlador.modificarPaciente); //modificar datos de un paciente por su dni
+router.put('/modificar/:dni', 
+[
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El correo no es válido').isEmail(),
+    check('email', 'El email es obligatorio').not().isEmpty(),
+    check('email').custom( emailPacienteExiste ),
+    check('edad','El campo edad no es correcto').isInt(),
+    check('edad', 'La edad es obligatoria').not().isEmpty(),
+    check('edad').custom(edadCorrecta),
+    check('telf', 'El teléfono debe ser numérico').isInt(),
+    check('telf', 'El teléfono es obligatorio').not().isEmpty(),
+    check('seguro', 'El seguro debe ser numérico').isInt(),
+    check('seguro', 'El número de seguro es obligatorio').not().isEmpty(),
+    validarCampos
+ ],
+controlador.modificarPaciente); //modificar datos de un paciente por su dni
 
 router.post('/generar/:cant', controlador.generarPacientes); // generar pacientes, especificando la cantidad
 
